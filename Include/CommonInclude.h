85aeb7b9719ea0277c58ac5b2d0d50cd7feaf90d
unknown <>
2016-03-23 18:27:40 +0800

    add commoninclude

---
+++ Include/CommonInclude.h
@@ -0,0 +1,27 @@
+
+#pragma once
+#include "stdafx.h"
+//windows头文件
+#include <Windows.h>
+
+//自定义头文件
+#include "CommonDefine.h"
+#include "VisionStruct.h"
+#include "CommonFunc.h"
+#include "logger.h"
+#include "CfgFileOp.h"
+#include "CvImage.h"
+#include "Thread.h"
+#include "Detector.h"
+
+//opencv头文件
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "cv.h"
+
+//标准模板头文件
+#include <map>
+#include <vector>
+
+using namespace cv;
+using namespace std;
