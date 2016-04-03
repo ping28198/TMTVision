9d91bf8743e0cd6af3c1fd7608aa73766a8cde1f
towanglei <towanglei@163.com>
2016-04-03 14:55:44 +0800

    添加文件夹监控工具类DirWatcher和服务类DirWatchServer, 分别实现同步监控和异步监控

---
+++ Include/DirWatcher.cpp
@@ -0,0 +1,151 @@
+#include "DirWatcher.h"
+#include "CommonFunc.h"
+
+//创建文件夹读取句柄, 初始化ReadDirectoryChangesW相关参数
+bool DirWatcher::RegPath(LONGWSTR path, DWORD action)
+{
+	HANDLE hDir = ::CreateFileW(path,
+		GENERIC_READ | FILE_LIST_DIRECTORY,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_BACKUP_SEMANTICS,
+		NULL);
+	if (INVALID_HANDLE_VALUE == hDir)
+	{
+		return false;
+		OutputDebugString(L"<DirWatcher::RegPath() failed.>\n");
+	}
+	m_hDir = hDir;
+	m_action = action;
+	CCommonFunc::SafeStringCpy(m_path, STR_LEN(m_path), path);
+	return true;
+}
+//执行前强制停止线程, 停止监控文件夹
+void DirWatcher::FreePath()
+{
+	if (m_hDir != 0)
+	{
+		CloseHandle(m_hDir);
+		m_hDir = 0;
+	}
+	m_path[0] = 0;
+}
+//供重载的操作函数,主函数会重复执行该函数
+bool DirWatcher::Watch(LONGWSTR& fileName, DWORD& action)
+{
+	if (m_hDir == 0)
+	{
+		fileName[0] = 0;
+		action = 0;
+		OutputDebugString(L"<DirWatcher::Watch() failed.>\n");
+		return false;
+	}
+	DWORD cbBytes;
+	DWORD notify[NOTIFYSIZE] = { 0 };
+	FILE_NOTIFY_INFORMATION *pnotify = (FILE_NOTIFY_INFORMATION*)notify;
+	if (ReadDirectoryChangesW(m_hDir,
+		&notify, NOTIFYSIZE, // Buffer and size
+		true, // monitor children?
+		m_action,
+		&cbBytes, NULL, NULL))
+	{
+		if (cbBytes <= 0)
+		{
+			OutputDebugString(L"<DirWatcher::Watch() Buffer overflow.>\n");
+			return true;
+		}
+		//设置类型过滤器,监听文件创建、更改、删除、重命名等
+		switch (pnotify->Action)
+		{
+		case FILE_ACTION_ADDED:
+			CCommonFunc::SafeStringCpy(fileName, STR_LEN(fileName), pnotify->FileName);
+			action = FILE_ACTION_ADDED;
+			OutputDebugString(L"<DirWatcher::Watch() FILE_ACTION_ADDED.>\n");
+			return true;
+			break;
+		case FILE_ACTION_REMOVED:
+			CCommonFunc::SafeStringCpy(fileName, STR_LEN(fileName), pnotify->FileName);
+			action = FILE_ACTION_REMOVED;
+			OutputDebugString(L"<DirWatcher::Watch() FILE_ACTION_REMOVED.>\n");
+			return true;
+			break;
+		case FILE_ACTION_MODIFIED:
+			CCommonFunc::SafeStringCpy(fileName, STR_LEN(fileName), pnotify->FileName);
+			action = FILE_ACTION_MODIFIED;
+			OutputDebugString(L"<DirWatcher::Watch() FILE_ACTION_MODIFIED.>\n");
+			return true;
+			break;	
+		case FILE_ACTION_RENAMED_OLD_NAME:
+			CCommonFunc::SafeStringCpy(fileName, STR_LEN(fileName), pnotify->FileName);
+			action = FILE_ACTION_RENAMED_OLD_NAME;
+			OutputDebugString(L"<DirWatcher::Watch() FILE_ACTION_RENAMED_OLD_NAME.>\n");
+			return true;
+			break;
+		case FILE_ACTION_RENAMED_NEW_NAME:
+			CCommonFunc::SafeStringCpy(fileName, STR_LEN(fileName), pnotify->FileName);
+			action = FILE_ACTION_RENAMED_NEW_NAME;
+			return true;
+			break;
+		default:
+			fileName[0] = 0;
+			action = 0;
+			OutputDebugString(L"<DirWatcher::Watch() Unknow action.>\n");
+			return false;
+			break;
+		}
+	}
+	fileName[0] = 0;
+	action = 0;
+	OutputDebugString(L"<DirWatcher::Watch() No action.>\n");
+	return false;
+}
+
+
+//创建文件夹读取句柄, 初始化ReadDirectoryChangesW相关参数
+//执行前强制停止线程, 需要用Create()再次启动
+bool DirWatchServer::RegPath(LONGWSTR path, DWORD action)
+{
+	EnterCriticalSection(&m_section);
+	m_fileNameQueue.Clear();
+	Thread::ForceEnd();
+	return DirWatcher::RegPath(path, action);
+	LeaveCriticalSection(&m_section);
+}
+//执行前强制停止线程, 停止监控文件夹
+void DirWatchServer::FreePath()
+{
+	EnterCriticalSection(&m_section);
+	Thread::ForceEnd();
+	DirWatcher::FreePath();
+	m_fileNameQueue.Clear();
+	LeaveCriticalSection(&m_section);
+}
+//先扫描注册文件夹所有文件, 文件名推入队列中保留最近的文件
+//再重载启动函数,内部添加FindFirstChangeNotification用于启动监控
+void DirWatchServer::Create(int times, long waiteTime)
+{
+	if (m_path[0] == 0 || m_hDir == 0)
+	{
+		OutputDebugString(L"<DirWatchServer: Create() failed.>\n");
+		return;
+	}
+    Thread::Create(times, waiteTime);
+}
+//重载启动函数
+void DirWatchServer::Destroy()
+{
+	FreePath();
+}
+//供重载的操作函数,主函数会重复执行该函数,内部调用Watch()函数,具有线程保护
+void DirWatchServer::Task()
+{	
+	FileItem tmpFileItem;
+	tmpFileItem.m_fileName[0] = 0;
+	tmpFileItem.m_fileAction = 0;
+	tmpFileItem.m_fileProcessed = false;
+	EnterCriticalSection(&m_section);
+	DirWatcher::Watch(tmpFileItem.m_fileName, tmpFileItem.m_fileAction);
+	m_fileNameQueue.ForcTail(tmpFileItem);
+	LeaveCriticalSection(&m_section);
+}
---
+++ Include/DirWatcher.h
@@ -0,0 +1,153 @@
+﻿///<proj_info>
+//==============================================================================
+// 项目名 ：智能监控
+// 文件名 ：DirWatcher.h
+// 作  者 ：王磊
+// 用  途 ：DirWatcher文件夹监控工具类,实现同步监控
+//          DirWatchServer文件夹监控线程类,实现异步监控
+// 版  权 ：霍比特人
+//==============================================================================
+///</proj_info>
+
+///<ver_info>
+// 版本记录	
+//==============================================================================
+//版本号  开发人员      时间      描述
+//1.0     王磊        2016.3.20  整合旧版本
+//2.0     王磊        2016.3.29  ReadDirectoryChangesW监控二级文件夹,并返回
+//                               最新文件信息,采用同步方式通知文件夹变化
+//2.1     王磊        2016.4.3   拆分为DirWatcher工具类和DirWatchServer线程类
+//                               分别实现同步监控和异步监控
+//==============================================================================
+///</ver_info>
+
+///<header_info>
+//==============================================================================
+//添加所需的头文件
+#pragma once
+#include "Thread.h"
+#include "CommonDefine.h"
+#include "Queue.h"
+//==============================================================================
+///</header_info>
+
+///<class_info>
+//==============================================================================
+//功能描述:文件夹监控类, 同步监控
+//         监视文件夹改变, 给出改变内容索引
+class DirWatcher
+{
+public:
+	//监控的路径
+	LONGWSTR m_path;
+	//监控的路径句柄
+	HANDLE m_hDir;
+	DWORD m_action;
+	enum { NOTIFYSIZE=2048 };
+	//DWORD m_notify[NOTIFYSIZE];
+	DirWatcher()
+	{
+		m_hDir = 0;
+		//初始化文件名队列
+		m_path[0]=0;
+		m_action = 0x00000000;
+	}
+	~DirWatcher()
+	{
+		FreePath();
+	}
+	//创建文件夹读取句柄, 初始化ReadDirectoryChangesW相关参数
+	//#define FILE_NOTIFY_CHANGE_FILE_NAME    0x00000001   
+	//#define FILE_NOTIFY_CHANGE_DIR_NAME     0x00000002   
+	//#define FILE_NOTIFY_CHANGE_ATTRIBUTES   0x00000004   
+	//#define FILE_NOTIFY_CHANGE_SIZE         0x00000008   
+	//#define FILE_NOTIFY_CHANGE_LAST_WRITE   0x00000010   
+	//#define FILE_NOTIFY_CHANGE_LAST_ACCESS  0x00000020   
+	//#define FILE_NOTIFY_CHANGE_CREATION     0x00000040   
+	//#define FILE_NOTIFY_CHANGE_SECURITY     0x00000100   
+	//#define FILE_ACTION_ADDED                   0x00000001   
+	//#define FILE_ACTION_REMOVED                 0x00000002   
+	//#define FILE_ACTION_MODIFIED                0x00000003   
+	//#define FILE_ACTION_RENAMED_OLD_NAME        0x00000004   
+	//#define FILE_ACTION_RENAMED_NEW_NAME        0x00000005 
+	virtual bool RegPath(LONGWSTR path,DWORD action= FILE_NOTIFY_CHANGE_LAST_WRITE);
+	//执行前强制停止线程, 停止监控文件夹
+	virtual void FreePath();
+	//调用ReadDirectoryChangesW用于等待监控消息
+	bool Watch(LONGWSTR& fileName,DWORD& action);
+};
+//==============================================================================
+///</class_info>
+
+///<algorithm_info>
+//==============================================================================
+//功能描述: 文件夹监控线程类, 异步监控(线程内同步,线程外异步):
+// 
+//         Store as an array
+//        ┏━━━━━━━┓
+//   ┏━▶┃  FileItem × ┃◀━p_DataList
+//   ┃   ┃  FileItem × ┃
+//   ┃   ┃  FileItem × ┃             Parent somewhere ◀━━━┓
+//  Index ┃  FileItem 0  ┃◀━ p_Head┏━DelHead()              ┃
+//  as a  ┃  FileItem 1  ┃       ◀━┛     ┗━━━━━━━━━┛
+//  ring  ┃       :      ┃
+//   ┃   ┃       :      ┃ 
+//   ┃   ┃       :      ┃              DirWatchServer::Task()◀┓    
+//   ┃   ┃  FileItem N  ┃◀━ p_Tail┏━ ForcTail()            ┃
+//   ┃   ┃  FileItem × ┃       ◀━┛     ┗━━━━━━━━━┛
+//   ┗━ ┃  FileItem × ┃
+//        ┗━━━━━━━┛
+//
+//==============================================================================
+///</algorithm_info>
+
+///<datastruct_info>
+//==============================================================================
+struct FileItem
+{
+	LONGWSTR m_fileName;
+	DWORD m_fileAction;
+	bool m_fileProcessed;
+};//==============================================================================
+  ///</datastruct_info>
+
+///<class_info>
+//==============================================================================
+//功能描述:文件夹监控线程类, 异步监控(线程内同步,线程外异步), 
+//         通过队列才线程内外交换数据, 
+//         线程内不断将最新变动的文件强制推入循环队列尾, 保持文件队列最新, 
+//         线程外(父线程)不断处理队列头文件, 处理完删除队列头.
+class DirWatchServer :
+	public Thread, public DirWatcher
+{
+public:
+	//文件名队列, 用于保存最近的历史文件, 
+    //处理线程将从头部读取执行释放
+	//监控线程将从尾部添加
+	enum { QUEUESIZE = 64 };
+	Queue<FileItem> m_fileNameQueue;
+	DirWatchServer(HANDLE  hParent = 0)
+	{
+		//初始化文件名队列
+		m_fileNameQueue.Initial(QUEUESIZE);
+	}
+	~DirWatchServer()
+	{
+		//卸载文件名队列
+		m_fileNameQueue.Unitial();
+	}
+	//创建文件夹读取句柄, 初始化ReadDirectoryChangesW相关参数
+	//执行前强制停止线程, 需要用Create()再次启动,具有线程保护
+	bool RegPath(LONGWSTR path, DWORD action = FILE_NOTIFY_CHANGE_LAST_WRITE);
+	//执行前强制停止线程, 停止监控文件夹,具有线程保护
+	void FreePath();
+	//先扫描注册文件夹所有文件, 文件名推入队列中保留最近的文件
+	//再重载启动函数,内部添加FindFirstChangeNotification用于启动监控
+	void Create(int times = -1, long waiteTime = 0);
+	//重载启动函数
+	void Destroy();
+	//供重载的操作函数,主函数会重复执行该函数,内部调用Watch()函数,具有线程保护
+	void Task();
+};
+//==============================================================================
+///</class_info>
