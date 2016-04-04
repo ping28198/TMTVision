bc09f3d9551576bd24a8f36ff3c0339b9c29e663
towanglei <towanglei@163.com>
2016-04-04 23:14:52 +0800

    添加识别算法虚基类, 内含尚未实现的派生类

---
+++ Include/Detector.cpp
@@ -0,0 +1,195 @@
+#include "Detector.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+//初始化资源和掩码图像
+bool BackgroundDetector::Initial(Tmtv_AlgorithmInfo *pAlgorithmInfo)
+{
+	Detector::Initial(pAlgorithmInfo);
+		///<BackgroundSubtractorMOG2初始化>
+
+
+
+
+
+
+		///</BackgroundSubtractorMOG2初始化>
+}
+//卸载资源和掩码图像
+void BackgroundDetector::Unitial()
+{
+	///<BackgroundSubtractorMOG2卸载>
+
+
+
+
+
+
+	///</BackgroundSubtractorMOG2卸载>
+	Detector::Unitial();
+}
+//识别当前图像队列
+bool BackgroundDetector::Detect(Mat & srcImageData, Mat & rectImageData, Tmtv_DefectInfo & defects, void * paras, long paraSize)
+{
+
+	return false;
+}
+
+
+//BackgroundDetector::BackgroundDetector()
+//{
+//}
+//
+//BackgroundDetector::~BackgroundDetector()
+//{
+//}
+//
+////初始化掩码图像
+//bool BackgroundDetector::Initial(int imageWidth, int imageHeight, Mat * pMaskImageData)
+//{
+//	return Detector::Initial(imageWidth, imageHeight, pMaskImageData);
+//}
+////初始化掩码图像
+//bool BackgroundDetector::Initial(LONGSTR maskImagePath)
+//{
+//	return Detector::Initial(maskImagePath);
+//}
+////卸载掩码图像
+//void BackgroundDetector::Unitial()
+//{
+//	Detector::Unitial();
+//}
+
+////识别当前图像队列
+//bool BackgroundDetector::Detect(Mat& srcImageData, Mat& rectImageData, vector<Rect>& rects, vector<double>& areas, double minArea, double learnRatio)
+//{
+//	////转换为灰度图像
+//	//Mat grayImg;
+//	//cv::cvtColor(srcImageData, grayImg, COLOR_BGR2GRAY);
+//	double resizeRatio = 1.0;
+//	//图像缩放减少计算量
+//	Mat *subImg=new Mat;
+//	Size dsize;
+//	cv::resize(srcImageData, *subImg, dsize, resizeRatio, resizeRatio, INTER_LINEAR);
+//	//高斯模糊减少平移影响
+//	Mat gaussImg;
+//	Size maskSize;
+//	maskSize.width = GAUSSIANSIZE;
+//	maskSize.height = GAUSSIANSIZE;
+//	cv::GaussianBlur(*subImg, gaussImg, dsize, GAUSSIANSIGMA, GAUSSIANSIGMA);
+//
+//	//背景监测算法
+//	p_backgroundSubtractor->apply(gaussImg, rectImageData, learnRatio);
+//	Mat RGBRectImageData[3];
+//	cv::split(rectImageData, RGBRectImageData);
+//	Mat RGBMaskImageData[3];
+//	cv::split(m_maskImageData, RGBMaskImageData);
+//	cv::min(RGBRectImageData[0], RGBMaskImageData[0], RGBMaskImageData[1]);
+//
+//	RGBMaskImageData[1].copyTo(rectImageData);
+//	Mat tmpImg;
+//	//开运算用来消除小物体,先腐蚀后膨胀
+//	cv::erode(rectImageData, tmpImg, Mat(MORPHSIZE1, MORPHSIZE1, CV_8U), Point(-1, -1), 1);
+//	cv::dilate(tmpImg, rectImageData, Mat(MORPHSIZE2, MORPHSIZE2, CV_8U), Point(-1, -1), 1);
+//	cv::erode(rectImageData, tmpImg, Mat(MORPHSIZE1, MORPHSIZE1, CV_8U), Point(-1, -1), 1);
+//	cv::dilate(tmpImg, rectImageData, Mat(MORPHSIZE2, MORPHSIZE2, CV_8U), Point(-1, -1), 1);
+//
+//	//闭运算用来填充物体内细小空洞,先膨胀后腐蚀
+//	cv::dilate(rectImageData, tmpImg, Mat(MORPHSIZE3, MORPHSIZE3, CV_8U), Point(-1, -1), 1);
+//	cv::erode(tmpImg, rectImageData, Mat(MORPHSIZE4, MORPHSIZE4, CV_8U), Point(-1, -1), 1);
+//	cv::dilate(rectImageData, tmpImg, Mat(MORPHSIZE3, MORPHSIZE3, CV_8U), Point(-1, -1), 1);
+//	cv::erode(tmpImg, rectImageData, Mat(MORPHSIZE4, MORPHSIZE4, CV_8U), Point(-1, -1), 1);
+//
+//	cv::dilate(rectImageData, tmpImg, Mat(MORPHSIZE5, MORPHSIZE5, CV_8U), Point(-1, -1), 1);
+//	cv::erode(tmpImg, rectImageData, Mat(MORPHSIZE6, MORPHSIZE6, CV_8U), Point(-1, -1), 1);
+//
+//	//寻找区域边缘
+//	vector<vector<Point> > contours;
+//
+//	cv::findContours(rectImageData, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+//
+//	//联合区域并消除小区域
+//	for (int i = 0; i < contours.capacity(); i++)
+//	{
+//		Rect rect = cv::boundingRect(contours.at(i));
+//		double area = cv::contourArea(contours.at(i));
+//		if (minArea > 0)
+//		{
+//			if (area < minArea) continue;
+//		}
+//		cv::rectangle(rectImageData, rect, Scalar(255, 255, 255), CV_FILLED);
+//	}
+//	cv::dilate(rectImageData, tmpImg, Mat(MORPHSIZE7, MORPHSIZE7, CV_8U), Point(-1, -1), 1);
+//	cv::erode(tmpImg, rectImageData, Mat(MORPHSIZE7, MORPHSIZE7, CV_8U), Point(-1, -1), 1);
+//
+//	//统计矩形区域
+//	vector<vector<cv::Point>> contours1;
+//	
+//	cv::findContours(rectImageData, contours1, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+//
+//	for (int i = 0; i < contours1.capacity(); i++)
+//	{
+//		Rect rect = cv::boundingRect(contours1.at(i));
+//		double area = cv::contourArea(contours1.at(i));
+//		if (minArea > 0)
+//		{
+//			if (area < minArea) continue;
+//		}
+//		cv::rectangle(rectImageData, rect, Scalar(255, 255, 255));
+//	}
+//	//统计矩形区域
+//	vector<vector<cv::Point> > contours2;
+//	cv::findContours(rectImageData, contours2, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+//	for (int i = 0; i < contours2.capacity(); i++)
+//	{
+//		Rect rect = cv::boundingRect(contours2.at(i));
+//		double area = cv::contourArea(contours2.at(i));
+//		if (minArea > 0)
+//		{
+//			if (area < minArea) continue;
+//		}
+//		cv::rectangle(rectImageData, rect, Scalar(255, 255, 255));
+//	}
+//	//统计矩形区域
+//	Mat tmpMaskImageData;
+//	rectImageData.copyTo(tmpMaskImageData);
+//	vector<vector<Point> > contours3;
+//	cv::findContours(tmpMaskImageData, contours3, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+//	rects.clear();
+//	areas.clear();
+//	for (int i = 0; i < contours3.capacity(); i++)
+//	{
+//		Rect rect = cv::boundingRect(contours3.at(i));
+//		rects.push_back(rect);
+//		double area = cv::contourArea(contours3.at(i));
+//		areas.push_back(area);
+//		cv::rectangle(srcImageData, rect, Scalar(0, 0, 255));
+//	}
+//
+//	delete subImg;
+//	return true;
+//}
+//
+//bool BackgroundDetector::Detect(char srcImagePath[256], char rectImagePath[256], double minArea, double learnRatio)
+//{
+//	vector<Rect> rects;
+//	vector<double> areas;
+//	Mat srcImageData = cv::imread(srcImagePath);
+//	Mat rectImageData;
+//	srcImageData.copyTo(rectImageData);
+//	if (srcImageData.empty())
+//	{
+//		return false;
+//	}
+//
+//	bool detected = Detect(srcImageData, rectImageData, rects, areas, minArea, learnRatio);
+//	if (detected)
+//	{
+//		if (rectImagePath[0] != 0)
+//		{
+//			cv::imwrite(rectImagePath, srcImageData);
+//		}
+//		return true;
+//	}
+//	return false;
+//}
---
+++ Include/Detector.h
@@ -0,0 +1,208 @@
+///<proj_info>
+//==============================================================================
+// 项目名 : 智能监控
+// 文件名 : Detector.h
+// 作  者 : 王磊
+// 用  途 : Detector图像检测算法接口类
+//          BackgroundDetector背景图像检测算法类
+// 版  权 : 霍比特人
+//==============================================================================
+///</proj_info>
+
+///<ver_info>
+// 版本记录	
+//==============================================================================
+//版本号  开发人员      时间      描述
+//1.0     王磊        2016.3.20  整合旧版本
+//2.0     王磊        2016.4.3   修改数据结构和算法参数,整合到接口类中
+//==============================================================================
+///</ver_info>
+
+///<header_info>
+//==============================================================================
+//添加所需的头文件
+#pragma once
+#include "cv.h"
+#include "opencv2/video/background_segm.hpp"
+#include "CommonDefine.h"
+#include "VisionStruct.h"
+using namespace std;
+using namespace cv;
+//==============================================================================
+///</header_info>
+
+///<class_info>
+//==============================================================================
+//功能描述: Detector图像检测算法接口类
+class Detector
+{
+public:
+	enum {SETTINGSIZE = 1024};
+public:
+	//预先准备的掩码图像
+	Mat *p_maskImageData;
+	int m_imageHeight;
+	int m_imageWidth;
+	//不同算法参数结构不同,派生自Tmtv_AlgorithmInfo结构
+	long algorithmInfoSize;
+	Tmtv_AlgorithmInfo *p_AlgorithmInfo;
+	Detector()
+	{
+		m_imageHeight = 0;
+		m_imageWidth = 0;
+		p_maskImageData = 0;
+		algorithmInfoSize = sizeof(Tmtv_AlgorithmInfo);
+		p_AlgorithmInfo = 0;
+	}
+	~Detector() { Unitial(); }
+	//初始化资源和掩码图像
+	bool Initial(Tmtv_AlgorithmInfo *pAlgorithmInfo)
+	{
+		if (pAlgorithmInfo == 0) return false;
+		if (pAlgorithmInfo->structSize < algorithmInfoSize) return false;
+		p_AlgorithmInfo = (Tmtv_AlgorithmInfo*)malloc(pAlgorithmInfo->structSize);
+		memcpy(p_AlgorithmInfo, pAlgorithmInfo, pAlgorithmInfo->structSize);
+		Mat tmpMat = cv::imread(p_AlgorithmInfo->MaskImgPath);
+		if (tmpMat.empty()) return false;
+		Size mskSize = tmpMat.size();
+		int mskDepth = tmpMat.depth();
+		int mskChannels = tmpMat.channels();
+		if (mskSize.height<4 || 
+			mskSize.width<4 ||
+			mskDepth!= CV_8U||
+			mskChannels!=1){return false;}
+		m_imageHeight = mskSize.height;
+		m_imageWidth = mskSize.width;
+		p_maskImageData = new Mat(m_imageHeight, m_imageWidth, CV_8UC1);
+		if (p_maskImageData != 0)
+		{
+			tmpMat.copyTo(*p_maskImageData);
+			return true;
+		}
+		return false;
+	}
+	//卸载资源和掩码图像
+	void Unitial() 
+	{
+		if (p_maskImageData != 0)
+		{
+			p_maskImageData->release();
+			delete p_maskImageData;
+			p_maskImageData = 0;
+		}
+		m_imageHeight = 0;
+		m_imageWidth = 0;
+		if (p_AlgorithmInfo != 0)
+		{
+			free(p_AlgorithmInfo);
+			//delete p_AlgorithmInfo;
+			p_AlgorithmInfo = 0;
+		}
+	}
+	//重设算法
+	void Reset(Tmtv_AlgorithmInfo *pAlgorithmInfo)
+	{
+		Unitial();
+		Initial(pAlgorithmInfo);
+	}
+	//识别当前图像队列
+	virtual bool Detect(Mat& srcImageData, Mat& rectImageData, 
+		Tmtv_DefectInfo & defects,
+		void* paras=0, long paraSize=0)=0;
+	//识别当前图像队列
+	bool Detect(LONGSTR srcImagePath, LONGSTR rectImagePath,
+		Tmtv_DefectInfo & defects,
+		void* paras = 0, long paraSize = 0)
+	{
+		Mat srcImageData = cv::imread(srcImagePath);
+		Mat rectImageData;
+		srcImageData.copyTo(rectImageData);
+		if (srcImageData.empty())
+		{
+			return false;
+		}
+		
+		bool detected = Detect(srcImageData, rectImageData, defects, paras, paraSize);
+		if (detected)
+		{
+			if (rectImagePath[0] != 0)
+			{
+				cv::imwrite(rectImagePath, srcImageData);
+			}
+			return true;
+		}
+		return false;
+	};
+};
+//==============================================================================
+///</class_info>
+
+///<datastruct_info>
+//==============================================================================
+struct Tmtv_BackgroundDetectorInfo: Tmtv_AlgorithmInfo	//算法信息
+{
+	int STRLEN = 260;
+	int GAUSSIANSIZE = 5;
+	double GAUSSIANSIGMA = 0.5;
+	int MORPHSIZE1 = 3;
+	int MORPHSIZE2 = 3;
+	int MORPHSIZE3 = 3;
+	int MORPHSIZE4 = 3;
+	int MORPHSIZE5 = 5;
+	int MORPHSIZE6 = 5;
+	int MORPHSIZE7 = 5;
+	int THEREHOLD = 30;
+	Tmtv_BackgroundDetectorInfo()
+	{
+		structSize = sizeof(Tmtv_BackgroundDetectorInfo);
+	};
+	~Tmtv_BackgroundDetectorInfo()
+	{
+		int a = 0;
+	}
+};
+//==============================================================================
+///</datastruct_info>
+
+
+
+///<class_info>
+//==============================================================================
+//功能描述: BackgroundDetector背景图像检测算法类
+class BackgroundDetector:public Detector
+{
+public:
+	BackgroundDetector()
+	{
+		m_imageHeight = 0;
+		m_imageWidth = 0;
+		p_maskImageData = 0;
+		algorithmInfoSize = sizeof(Tmtv_BackgroundDetectorInfo);
+		p_AlgorithmInfo = 0;
+	}
+	~BackgroundDetector() { Unitial(); }
+	//初始化资源和掩码图像
+	bool Initial(Tmtv_AlgorithmInfo *pAlgorithmInfo);
+	//卸载资源和掩码图像
+	void Unitial();
+	//重设算法
+	void Reset(Tmtv_AlgorithmInfo *pAlgorithmInfo)
+	{
+		Unitial();
+		Initial(pAlgorithmInfo);
+	}
+	
+
+	Ptr<BackgroundSubtractorMOG2> p_backgroundSubtractor;
+	struct RunningPara
+	{
+		double minArea;
+		double learnRatio;
+	};
+	//识别当前图像队列
+	bool Detect(Mat& srcImageData, Mat& rectImageData,
+		Tmtv_DefectInfo & defects,
+		void* paras = 0, long paraSize = 0);
+};
+//==============================================================================
+///</class_info>
