c179ce2eea71fc8542afcab2c9cdc6766a51c76a
Ren Weiping <ping28198@126.com>
2016-04-05 19:27:16 +0800

    调整CommonInclude.h的包含顺序，避免winsock2.h和windows.h冲突

--- Include/CommonInclude.h
+++ Include/CommonInclude.h
@@ -2,9 +2,10 @@
 #pragma once
 #include "stdafx.h"
 //windows头文件
-#include <Windows.h>
+
 
 //自定义头文件
+#include "TmtSocket.h"//socket包含需要放在最前面
 #include "CommonDefine.h"
 #include "VisionStruct.h"
 #include "CommonFunc.h"
@@ -14,7 +15,7 @@
 #include "Thread.h"
 #include "Detector.h"
 #include "MemoryFile.h"
-#include "TmtSocket.h"
+
 #include "TmtCrypt.h"
 //opencv头文件
 #include "opencv2/imgproc/imgproc.hpp"
