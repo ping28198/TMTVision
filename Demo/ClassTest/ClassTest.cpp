3d15abeccbfb29f0c34c789046576cb4e3648bec
Ren Weiping <ping28198@126.com>
2016-04-08 11:10:35 +0800

    合并DemoRen到Demo

--- Demo/ClassTest/ClassTest.cpp
+++ Demo/ClassTest/ClassTest.cpp
@@ -2,40 +2,40 @@
 //
 
 #include "stdafx.h"
-#include "DirWatcher.h"
-#include "CommonFunc.h"
 
-#   define COL(x)  "\033[;" #x "m"  
-#   define RED     COL(31)  
-#   define GREEN   COL(32)  
-#   define YELLOW  COL(33)  
-#   define BLUE    COL(34)  
-#   define MAGENTA COL(35)  
-#   define CYAN    COL(36)  
-#   define WHITE   COL(0)  
-#   define GRAY    "\033[0m"  
+#include "CommonInclude.h"
+#include "DirWatcher.h"
+
+
+
+
+
 
 int main()
 {
-	//setlocale(LC_ALL, "chs");
-	DirWatchServer dirWatchServer;
-	dirWatchServer.RegPath(L"D:\\TST\\");
-	dirWatchServer.Create();
-	dirWatchServer.Resume();
-	HUGEWSTR  wstring;
-	HUGEWSTR  wstring2;
-	HUGESTR  string;
-	while (1)
+	DirWatchServer DirWatchSvr;
+	LONGWSTR myDir=L"D:\\a";
+	DirWatchSvr.RegPath(myDir);
+	DirWatchSvr.Create();
+	DirWatchSvr.Resume();
+	while (true)
 	{
-		wstring[0] = 0;
-		wstring2[0] = 0;
-		dirWatchServer.ToString(wstring,2,33);
-		/*CCommonFunc::SafeStringPrintf(wstring, TMTV_HUGESTRLEN, L"\033[;32m%s\033[0m\n", wstring2);*/
-		CCommonFunc::UnicodeToAnsi(wstring, string, TMTV_HUGESTRLEN);
-		system("cls");
-		printf(string);
+		cout<<DirWatchSvr.m_fileNameQueue.GetLength()<<endl;
+		if (DirWatchSvr.m_fileNameQueue.GetTail()==NULL)
+		{
+			Sleep(1000);
+			continue;
+		}
+		cout << DirWatchSvr.m_fileNameQueue.GetTail()->m_fileName<<endl;
+		cout << DirWatchSvr.m_fileNameQueue.GetTail()->m_fileAction<<endl;
+
+		cout << DirWatchSvr.m_fileNameQueue.GetData(DirWatchSvr.m_fileNameQueue.GetLength()-2)->m_fileName << endl;
+		cout << DirWatchSvr.m_fileNameQueue.GetData(DirWatchSvr.m_fileNameQueue.GetLength() - 2)->m_fileAction << endl;
 		Sleep(1000);
 	}
+	
+
+
     return 0;
 }
 
