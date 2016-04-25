2c3d815ff12b82c4cb8884709d3b07abe99c827e
Stanley-Tian <grasslight@qq.com>
2016-04-25 18:56:36 +0800

    增加monitor选项

--- WebApp/views/site/index.php
+++ WebApp/views/site/index.php
@@ -21,14 +21,9 @@ $this->title = 'TMT vision';
                 <p><a class="btn btn-default" href="<?=Yii::$app->homeUrl?>?r=camera/index">点击查看&raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <h2>监控图像查看</h2>
+                <p><img class="" src="<?= Yii::$app->getUrlManager()->getBaseUrl()?>/../resources/images/camera_sample.jpg" width="100%" alt=""/></p>
+                <p><a class="btn btn-default" href="<?=Yii::$app->homeUrl?>?r=monitor/index">点击查看&raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
